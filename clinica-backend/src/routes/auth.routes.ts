39db78cc366dc3889c29d5078088adffeab28012
M.Mazzulli <38134803+mmazzulli@users.noreply.github.com>
2025-08-28 17:11:54 +0100

    deletando files

--- clinica-backend/src/routes/auth.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { login, refreshToken, me } from '../controllers/auth.controller';
-export const authRouter = Router();
-authRouter.post('/login', login);
-authRouter.post('/refresh', refreshToken);
-authRouter.get('/me', me); // protected via header in controller for simplicity (ou use middleware)
--- clinica-backend/src/server.ts
+++
@@ -1,5 +0,0 @@
-import { app } from './app';
-import { env } from './env';
-app.listen(env.port, () => {
-  console.log(`API running on http://localhost:${env.port}`);
-});
