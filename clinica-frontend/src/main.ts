ca943f35960e1c4af8ea1bd659fc3135aa4518fe
M.Mazzulli <38134803+mmazzulli@users.noreply.github.com>
2025-09-08 15:35:53 +0100

    Criando o Frontend

--- clinica-frontend/src/main.ts
+++ clinica-frontend/src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { authInterceptor } from './app/services/auth.interceptor';
-import { AuthService } from './app/services/auth.service';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    AuthService, // garante que o serviço exista
-    provideHttpClient(withInterceptors([authInterceptor]))
-  ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
